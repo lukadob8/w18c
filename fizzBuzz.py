52da28a91533dbc7c7cffb432fed3b8b95f0a662
Luka Dobrijevic <lukadob@Lukas-MacBook-Pro.local>
2020-10-24 17:02:58 -0600

    Made fizzBuzz function

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,1 +1,14 @@
-numbers = [3, 30, 9, 10, 45, 10, 55, 99, 100, 87, 34, 33, 111, 113, 6]
+numbers = [3, 30, 9, 10, 45, 10, 55, 99, 100, 88, 34, 33, 111, 113, 6]
+
+def fizzBuzz(number):
+        if(number % 3 == 0 and number % 5 == 0):
+            print('fizzBuzz')
+        elif(number % 3 == 0):
+            print('fizz')
+        elif(number % 5 == 0):
+            print('buzz')
+        else:
+            print("Not Divisible by 3 or 5")
+
+for number in numbers:
+    fizzBuzz(number)
